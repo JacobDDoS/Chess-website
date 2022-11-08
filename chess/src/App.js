21a95f680f18b862bd4d129fa3b002479b3c339a
JacobDDoS <jacdimmitt@gmail.com>
2022-11-07 21:59:50 -0600

    Started working on chess board, added initial backrank black pieces

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,9 +1,14 @@
+import Board from "./Board/Board";
+
 function App() {
-  return (
+  return <>
     <div>
-      <h1>Hello World!</h1>
+      <h1>This is App!</h1>
     </div>
-  );
+    <div>
+      <Board/>
+    </div>
+  </>;
 }
 
 export default App;
---
+++ chess/src/Board/Board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div>
+        <h2>This is the board!!!</h2>
+    </div>
+  )
+}
+
+export default Board
